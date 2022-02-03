b036190058398e7ad6d5976193cb089d7d155344
Fabien Spindler <Fabien.Spindler@inria.fr>
2022-02-03 08:47:50 +0100

    Fix realsense sample

--- sensor/sample-vpRealSense2-5.cpp
+++ sensor/sample-vpRealSense2-5.cpp
@@ -4,7 +4,7 @@ int main() {
 #ifdef VISP_HAVE_REALSENSE2
   vpRealSense2 rs;
   rs.open();
-  td::cout << "RealSense sensor characteristics: \n" << rs << std::endl;
+  std::cout << "RealSense sensor characteristics: \n" << rs << std::endl;
 #endif
   return 0;
 }

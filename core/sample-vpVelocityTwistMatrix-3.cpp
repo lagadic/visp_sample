1587f309e1088146a6317024786ce944028572d0
Fabien Spindler <Fabien.Spindler@inria.fr>
2022-02-08 12:20:16 +0100

    Fix build with cxx standard 98

--- core/sample-vpVelocityTwistMatrix-3.cpp
+++ core/sample-vpVelocityTwistMatrix-3.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+#if (VISP_CXX_STANDARD >= VISP_CXX_STANDARD_11)
   {
   vpTranslationVector cte(0.1, 0.2, 0.3);
   vpRotationMatrix cRe( {0,  0, -1,
@@ -18,4 +19,5 @@ int main()
   vpVelocityTwistMatrix cVe(cRe);
   std::cout << "cVe:\n" << cVe << std::endl;
   }
+#endif
 }

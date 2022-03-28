1fa0a249e88f3dce27422f0c29018d6d88f25a9f
Fabien Spindler <Fabien.Spindler@inria.fr>
2022-03-28 17:21:03 +0200

    New sample

---
+++ core/sample-vpImagePoint-4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <visp3/core/vpImagePoint.h>
+
+int main()
+{
+  vpImagePoint start_pixel(10, 12);
+  vpImagePoint end_pixel(20, 16);
+  //vpImagePoint start_pixel(10.5, 12.2);
+  //vpImagePoint end_pixel(20, 12);
+  //vpImagePoint end_pixel(20.5, 16.2);
+  //vpImagePoint end_pixel(21, 16.4);
+  
+  for (auto curr_pixel = start_pixel; curr_pixel.inSegment(start_pixel, end_pixel);
+       curr_pixel = curr_pixel.nextInSegment(start_pixel, end_pixel)) {
+    std::cout << "pixel: " << curr_pixel << std::endl;
+    if (curr_pixel == end_pixel) break;
+  }
+  
+  return EXIT_SUCCESS;
+}

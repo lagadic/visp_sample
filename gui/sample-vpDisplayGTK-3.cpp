ae6972baddad08b9644104d2a54c3d2039bfd643
Fabien Spindler <Fabien.Spindler@inria.fr>
2023-05-19 11:50:09 +0200

    Use displayText() instead of deprecated displayCharString()

--- gui/sample-vpDisplayGTK-3.cpp
+++ gui/sample-vpDisplayGTK-3.cpp
@@ -16,10 +16,10 @@ int main()
   vpDisplay::display(I);
   std::string text;
   bool motion_allowed = false;
-  center.set_u( I.getWidth()/2  );
-  center.set_v( I.getHeight()/2 );
+  center.set_u(I.getWidth() / 2);
+  center.set_v(I.getHeight() / 2);
 
-  for ( ; ; ) {
+  for (; ; ) {
     vpDisplay::display(I);
     //vpDisplay::setBackground(I, vpColor::black);
     pos_text.set_u(30);
@@ -28,52 +28,52 @@ int main()
       text = "A left click to stop target motion...";
     else
       text = "A left click to start target motion...";
-    vpDisplay::displayCharString(I, pos_text, text, vpColor::red);
+    vpDisplay::displayText(I, pos_text, text, vpColor::red);
     pos_text.set_u(30);
     pos_text.set_v(45);
     text = "A right click to quit...";
-    vpDisplay::displayCharString(I, pos_text, text, vpColor::red);
+    vpDisplay::displayText(I, pos_text, text, vpColor::red);
 
-    if (vpDisplay::getPointerMotionEvent (I, pointer ) ) {
+    if (vpDisplay::getPointerMotionEvent(I, pointer)) {
       if (motion_allowed) {
         center = pointer;
         //std::cout << "focus: " << pointer << std::endl;
       }
     }
-    ip1.set_u( center.get_u() );
-    ip1.set_v( center.get_v() - 6*radius );
-    ip2.set_u( center.get_u() );
-    ip2.set_v( center.get_v() - 4*radius );
+    ip1.set_u(center.get_u());
+    ip1.set_v(center.get_v() - 6 * radius);
+    ip2.set_u(center.get_u());
+    ip2.set_v(center.get_v() - 4 * radius);
     vpDisplay::displayLine(I, ip1, ip2, vpColor::white, true);
-    ip1.set_u( center.get_u() - 6*radius );
-    ip1.set_v( center.get_v());
-    ip2.set_u( center.get_u() - 4*radius );
-    ip2.set_v( center.get_v() );
+    ip1.set_u(center.get_u() - 6 * radius);
+    ip1.set_v(center.get_v());
+    ip2.set_u(center.get_u() - 4 * radius);
+    ip2.set_v(center.get_v());
     vpDisplay::displayLine(I, ip1, ip2, vpColor::white, true);
-    ip1.set_u( center.get_u() );
-    ip1.set_v( center.get_v() + 6*radius );
-    ip2.set_u( center.get_u() );
-    ip2.set_v( center.get_v() + 4*radius );
+    ip1.set_u(center.get_u());
+    ip1.set_v(center.get_v() + 6 * radius);
+    ip2.set_u(center.get_u());
+    ip2.set_v(center.get_v() + 4 * radius);
     vpDisplay::displayLine(I, ip1, ip2, vpColor::white, true);
-    ip1.set_u( center.get_u() + 6*radius );
-    ip1.set_v( center.get_v());
-    ip2.set_u( center.get_u() + 4*radius );
-    ip2.set_v( center.get_v() );
+    ip1.set_u(center.get_u() + 6 * radius);
+    ip1.set_v(center.get_v());
+    ip2.set_u(center.get_u() + 4 * radius);
+    ip2.set_v(center.get_v());
     vpDisplay::displayLine(I, ip1, ip2, vpColor::white, true);
-    vpDisplay::displayCircle(I, center, 2*radius, vpColor::white, true);
-//     vpDisplay::displayCircle(I, center, 1.5*radius, vpColor::black, true);
-//     vpDisplay::displayCircle(I, center, radius, vpColor::white, true);
-    // vpDisplay::displayCircle(I, center, 5*radius, vpColor::white, false);
+    vpDisplay::displayCircle(I, center, 2 * radius, vpColor::white, true);
+    // vpDisplay::displayCircle(I, center, 1.5 * radius, vpColor::black, true);
+    // vpDisplay::displayCircle(I, center, radius, vpColor::white, true);
+    // vpDisplay::displayCircle(I, center, 5 * radius, vpColor::white, false);
 
     if (vpDisplay::getClick(I, dummy, button, false)) {
       // A click event was detected
       if (button == vpMouseButton::button3)
-  break;
+        break;
       else if (button == vpMouseButton::button1) {
-  if (motion_allowed)
-    motion_allowed = false;
-  else
-    motion_allowed = true;
+        if (motion_allowed)
+          motion_allowed = false;
+        else
+          motion_allowed = true;
       }
     }
     vpDisplay::flush(I);

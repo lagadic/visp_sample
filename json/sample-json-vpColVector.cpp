c11c7874a2ccfd7dea180dd0ed9da1a0cfe8f6cb
Fabien Spindler <Fabien.Spindler@inria.fr>
2023-05-22 21:55:19 +0200

    Fix compat with cxx98 standard

--- json/sample-json-vpColVector.cpp
+++ json/sample-json-vpColVector.cpp
@@ -5,7 +5,15 @@ int main()
 #if defined(VISP_HAVE_NLOHMANN_JSON)
   std::string filename = "col-vector.json";
   {
+#if (VISP_CXX_STANDARD >= VISP_CXX_STANDARD_11)
     vpColVector v({ 1, 2, 3, 4 });
+#else
+    vpColVector v(4);
+    v[0] = 1;
+    v[1] = 2;
+    v[2] = 3;
+    v[3] = 4;
+#endif
     std::ofstream file(filename);
     const nlohmann::json j = v;
     file << j;
